4e73326f8038f246100b53addaad5023f545da5d
Samet Dağ <samet822@hotmail.com>
2020-09-26 13:25:42 +0300

    Initial commit

---
+++ src/main/java/paket/citizen/incidentservice/IncidentServiceApplication.java
@@ -0,0 +1,13 @@
+package paket.citizen.incidentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paket/citizen/incidentservice/IncidentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package paket.citizen.incidentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
